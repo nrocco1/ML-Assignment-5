f2af37162b9d28f227c8d190f75f05251b416ced
Nick Rocco <nrocco1@nd.edu>
2018-04-16 19:40:09 -0400

    part 1 fixed

--- HW5-Q1.py
+++ HW5-Q1.py
@@ -8,8 +8,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+weight_vector = [0.01, 0.01]
+
 def target(x):
-    val = -1 + float(x[1]) + float(x[2])
+    val = -1 + float(x[0]) + float(x[1])
     if val > 0:
         return 1
     else:
@@ -21,46 +23,25 @@ def direction(x):
     else:
         return -1
 
-def gradient_descent(data, learning_rate, ec):
-    count = 0
+def gradient_descent(data, learning_rate, ec, n_iters):
+    global weight_vector
     iter_error = []
     sse = 0
     error = 0
-    weight_vector = [0.01, 0.01, 0.01]
-    delta_w = 0
-    for entry in data:
-        t = target(entry)
-        o = direction(np.dot(entry, weight_vector))
-        count += 1
-        if ec:
-            learning_rate = learning_rate/count
-        delta_w += learning_rate * (t-o) * entry
+    for i in range(n_iters):
+        error = 0
+        delta_w = [0,0]
+        for entry in data:
+            t = target(entry)
+            o = direction(np.dot(entry, weight_vector))
+            if ec:
+                learning_rate = learning_rate/(i+1)
+            delta_w += learning_rate * (t-o) * entry
+            error += (t-o)*(t-o)
         weight_vector += delta_w
-        error += (t-o)*(t-o)
-        if count == 5:
-            iter_error.append([count, error])
-        elif count == 10:
-            iter_error.append([count, error])
-        elif count == 20:
-            iter_error.append([count, error])
-        elif count == 50:
-            iter_error.append([count, error])
-        elif count == 100:
-            iter_error.append([count, error])
-        elif count == 500:
-            iter_error.append([count, error])
-        elif count == 1000:
-            iter_error.append([count, error])
-        elif count == 2000:
-            iter_error.append([count, error])
-        elif count == 3000:
-            iter_error.append([count, error])
-        elif count == 4000:
-            iter_error.append([count, error])
-        elif count == 5000:
-            iter_error.append([count, error])
     
-    return iter_error
+    print [n_iters, error] 
+    return [n_iters, error]
 
 if __name__ == '__main__':
     x_min_range = -1000
@@ -68,9 +49,14 @@ if __name__ == '__main__':
     N = 5000
     m = 2
     data = np.random.uniform(x_min_range,x_max_range,size=[N,m])
-    data = np.concatenate((np.ones([N,1]),data),axis=1)
 
-    iter_error = gradient_descent(data, 0.01, True)
+    iters = [5, 10, 20, 50, 100, 500, 1000, 2000, 3000, 4000, 5000]
+    
+    iter_error = []
+
+    for x in iters:
+        l = gradient_descent(data, 0.001, False, x)
+        iter_error.append(l)
 
     plt.plot([x[0] for x in iter_error], [y[1] for y in iter_error])
     plt.xlabel('Number of iterations')
